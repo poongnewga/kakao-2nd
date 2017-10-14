bb8865c263796af3a5c3819819038cd737eea8eb
Hee Jae Chae <poongnewga@gmail.com>
2017-10-14 16:58:44 +0900

    sav

--- index.js
+++ index.js
@@ -15,14 +15,6 @@ var ids_string2;
 var req_body;
 var features_arr;
 
-//
-// axios.get(base_url + "/token/" + login_token)
-//   .then(function (res) {
-//     console.log(res.data);
-//   })
-//   .catch(function (err) {
-//     console.log(err);
-//   });
 
 // GET submit_token : 이렇게 얻은 제출용 토큰은 10분동안 유효하다.
 async function CallTokenAPI() {
@@ -58,58 +50,62 @@ async function CallSeedAPI() {
 // 리스트가 나타나지 않을 경우 계속해서 호출하는 과정이 필요하다.
 async function CallDocumentAPI() {
   try {
+    // 카테고리별로 반복한다.
+    for (var i=0; i<=0; i++) {
 
-    var this_url = doc_ids_arr[0]
-    var res = await axios.get(base_url + doc_ids_arr[0], config);
+      var this_url = doc_ids_arr[i]
+      var res = await axios.get(base_url + doc_ids_arr[i], config);
 
-    var data = res.data;
-    var next_url = data.next_url;
+      var data = res.data;
+      var next_url = data.next_url;
 
-    if (next_url != this_url) {
-      console.log("OK, Let's get it!");
-      console.log();
+      if (next_url != this_url) {
+        console.log("OK, Let's get it!");
+        console.log("Try : " + doc_ids_arr[i]);
+        console.log();
 
-      var img_arr = data.images;
-      // console.log(next_url);
-      // console.log(img_arr);
+        var img_arr = data.images;
+        // console.log(next_url);
+        // console.log(img_arr);
 
-      // 중복 제거 작업
-      var img_obj = {};
-      for (var element of img_arr) {
-        img_obj[`${element.id}`] = element.type;
-      }
-      // 초기화
-      ids_string = "id=";
-      ids_string2 = "id=";
-      // add 인 경우만 골라서 스트링 생성
-      var cnt = 0;
-      for (var prop in img_obj) {
-        if (cnt < 50) {
-          if (img_obj[prop] == 'add') {
-            ids_string += (prop + ',');
+        // 중복 제거 작업
+        var img_obj = {};
+        for (var element of img_arr) {
+          img_obj[`${element.id}`] = element.type;
+        }
+        // 초기화
+        ids_string = "id=";
+        ids_string2 = "id=";
+        // add 인 경우만 골라서 스트링 생성
+        var cnt = 0;
+        for (var prop in img_obj) {
+          if (cnt < 50) {
+            if (img_obj[prop] == 'add') {
+              ids_string += (prop + ',');
+              cnt++;
+            }
+          } else {
+            ids_string2 += (prop + ',');
             cnt++;
           }
-        } else {
-          ids_string2 += (prop + ',');
-          cnt++;
         }
+        // console.log(ids_string);
+        // console.log(ids_string[ids_string.length-1]);
+        ids_string = ids_string.slice(0, ids_string.length-1);
+        ids_string2 = ids_string.slice(0, ids_string2.length-1);
+        // console.log(ids_string);
+
+
+        } else {
+          console.log("Let me do it again");
+          console.log();
+          res = await axios.get(base_url + doc_ids_arr[i], config);
+        }
+
+
+
+
       }
-      // console.log(ids_string);
-      // console.log(ids_string[ids_string.length-1]);
-      ids_string = ids_string.slice(0, ids_string.length-1);
-      ids_string2 = ids_string.slice(0, ids_string2.length-1);
-      // console.log(ids_string);
-
-    } else {
-      console.log("Let me do it again");
-      console.log();
-      res = await axios.get(base_url + doc_ids_arr[0], config);
-    }
-
-
-
-
-
 
   } catch (err) {
     console.log(err);
@@ -117,22 +113,18 @@ async function CallDocumentAPI() {
 }
 
 
-// Feature를 가져온다.
-
-async function GetFeatures() {
+// Add 할 대상에 대해서만! Feature를 가져온다.
+// Save에 사용할 features_arr을 리턴한다.
+async function GetFeatures(IDs_string) {
 
   try {
-    const res = await axios.get(base_url + "/image/feature?" + ids_string, config);
+
+    const res = await axios.get(base_url + "/image/feature?" + IDs_string, config);
     var data = res.data;
     // console.log(res.data);
     features_arr = data.features;
-    req_body = {
-      headers: {
-          "X-Auth-Token": submit_token
-        },
-      "data": features_arr
-    };
-    // console.log(req_body);
+    return features_arr;
+
   } catch (err) {
     console.log(err);
   }
@@ -187,7 +179,7 @@ async function DoAll() {
   // await CallTokenAPI();
 
   // 10분 전에 다시 시도할 때는 이걸 사용
-  submit_token = "Tdg4VkvynNl1JcX8-6kZ6b39vf-7Dw8bJ3ZKlupvW-gdXqRGDAJXJ36jvZqM3iVZ9KrVY8E";
+  submit_token = "TnypdWZVnglNYI1VkpXep3MbqT6Ep1yWeRGOxSjyzknK0D9GrfBjBvgZJ6z3vUG-3lK8rkX";
   config.headers["X-Auth-Token"] = submit_token;
 
   await CallSeedAPI();
